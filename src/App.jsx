e24635fae285cb8da127381d32975853db4feaf1
Deepak911741 <dk367748@gmail.com>
2024-07-31 15:55:48 +0530

    contex api

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ContextMain from './context/components/ContextMain'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <ContextMain />
+      </div>
+    </>
+  )
+}
+
+export default App
